78e36afea08d9badae70e448e2be2a81d466d5c2
Rjrebel <102338080+Rjrebel@users.noreply.github.com>
2022-07-15 09:33:13 +0530

    version 2

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ var logInPage = document.getElementById('log-in-page');
 var gameSection = document.getElementById('game-section');
 var resultSection = document.getElementById('result-section');
 var hint = document.getElementById('result');
-var startBtn = document.getElementById('start-btn');
+var lastGuess = document.getElementById('last-guess');
+var startBtn = document.getElementById('button');
 var numberCnter = document.getElementById('number-container');
 var submit = document.querySelector('.submit');
 
@@ -32,21 +33,34 @@ submit.addEventListener('click', function(e){
     console.log(x , y);
     if(x != y ){
         if(x > y){
-            hint.textContent = "Your guess the wrong number, try larger number."
+            hint.textContent = "(Hint : try LARGER number than your previously guessed.)"
             hint.style.display = "block";
+            numberCnter.value = "";
+            alert("You guessed it wrong, try again.")
         } else{
-            hint.textContent = "Your guess the wrong number, try smaller number."
+            hint.textContent = "(Hint : try SMALLER number than your previously guessed.)"
             hint.style.display = "block";
+            numberCnter.value = "";
+            alert("You guessed it wrong, try again.")
         }
+        lastGuess.textContent = "Your last guessed number : "+ y;
+        numberCnter.style.backgroundColor = "#f74d4d";
     } else {
         msg.textContent = firstName + ", You took "+ chances +" chances to win the game"
         gameSection.style.display = "none";
         resultSection.style.display = "block";
     }
     } else {
-        alert("Please, enter number between 1 to 100.")
+        alert("Please, enter number between 0 to 100.");
+        numberCnter.value = "";
         e.preventDefault();
     }
     
+});
+
+numberCnter.addEventListener('focus', function(){
+    numberCnter.style.backgroundColor = "white";
+    hint.style.display = "none";
+    lastGuess.textContent = "";
 });
 
